539a4500009d182168b6d1dccaa39d8b018b9e89
hunjixin <1084400399@qq.com>
2023-09-28 22:54:50 +0800

    use net cn to detect public ip

--- main.go
+++ main.go
@@ -4,8 +4,12 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"regexp"
 	"time"
 
 	alidns20150109 "github.com/alibabacloud-go/alidns-20150109/v4/client"
@@ -19,6 +23,8 @@ var (
 	secret   = flag.String("secret", "", "")
 	domain   = flag.String("domain", "", "")
 	interval = flag.String("interval", "10m", "")
+
+	iprex = regexp.MustCompile(`(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}`)
 )
 
 func createClient(accessKeyId *string, accessKeySecret *string) (_result *alidns20150109.Client, _err error) {
@@ -34,7 +40,7 @@ func createClient(accessKeyId *string, accessKeySecret *string) (_result *alidns
 }
 
 // resolvePublicIp resolve public ip by ident me
-func resolvePublicIp(ctx context.Context) (string, error) {
+func resolvePublicIpByIdentMe(ctx context.Context) (string, error) {
 	resp, err := http.Get("https://ident.me")
 	if err != nil {
 		return "", err
@@ -45,6 +51,25 @@ func resolvePublicIp(ctx context.Context) (string, error) {
 		return "", err
 	}
 	return root.Body().InternalNode().FirstChild.Data, nil
+}
+
+// resolvePublicIp resolve public ip by ident me
+func resolvePublicIpByNetCN(ctx context.Context) (string, error) {
+	resp, err := http.Get("http://www.net.cn/static/customercare/yourip.asp")
+	if err != nil {
+		return "", err
+	}
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	submatchall := iprex.FindAllString(string(data), 1)
+	if len(submatchall) == 0 {
+		return "", fmt.Errorf("www.net.cn maybe fail")
+	}
+	return submatchall[0], nil
 }
 
 // run get ip and set to aliyun dns server
@@ -65,13 +90,15 @@ func run(ctx context.Context) error {
 	if err != nil {
 		return err
 	}
+	recordId := ""
 	preIP := ""
 	if len(records.Body.DomainRecords.Record) > 0 {
 		preIP = *records.Body.DomainRecords.Record[0].Value
+		recordId = *records.Body.DomainRecords.Record[0].RecordId
 	}
 
 	for {
-		ip, err := resolvePublicIp(ctx)
+		ip, err := resolvePublicIpByNetCN(ctx)
 		if err != nil {
 			log.Println("get public ips ", err)
 			goto SLEEP
@@ -79,18 +106,35 @@ func run(ctx context.Context) error {
 
 		if ip != preIP {
 			log.Printf("ip changed old(%s) new(%s) try to change dns", preIP, ip)
-			_, err = client.AddDomainRecord(&alidns20150109.AddDomainRecordRequest{
-				DomainName:   domain,
-				Lang:         tea.String("en"),
-				RR:           tea.String("@"),
-				Type:         tea.String("A"),
-				UserClientIp: tea.String(ip),
-				Value:        tea.String(ip),
-			})
-			if err != nil {
-				log.Println("request error", err)
-				goto SLEEP
+			if len(recordId) == 0 {
+				adrResp, err := client.AddDomainRecord(&alidns20150109.AddDomainRecordRequest{
+					DomainName:   domain,
+					Lang:         tea.String("en"),
+					RR:           tea.String("@"),
+					Type:         tea.String("A"),
+					UserClientIp: tea.String(ip),
+					Value:        tea.String(ip),
+				})
+				if err != nil {
+					log.Println("add record error", err)
+					goto SLEEP
+				}
+				recordId = *adrResp.Body.RecordId
+			} else {
+				_, err = client.UpdateDomainRecord(&alidns20150109.UpdateDomainRecordRequest{
+					Lang:         tea.String("en"),
+					RR:           tea.String("@"),
+					RecordId:     tea.String(recordId),
+					Type:         tea.String("A"),
+					UserClientIp: tea.String(ip),
+					Value:        tea.String(ip),
+				})
+				if err != nil {
+					log.Println("update record error", err)
+					goto SLEEP
+				}
 			}
+
 			preIP = ip
 			log.Println("dns updated successfully")
 			goto SLEEP
@@ -102,6 +146,8 @@ func run(ctx context.Context) error {
 }
 
 func main() {
+	os.Setenv("HTTPS_PROXY", "")
+	os.Setenv("HTTP_PROXY", "")
 	flag.Parse()
 	ctx := context.Background()
 	err := run(ctx)
